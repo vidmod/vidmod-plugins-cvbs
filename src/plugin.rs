83c3878f8fd8a3517e6cf3bc31b146b54935649a
chordtoll <git@chordtoll.com>
2022-03-09 22:00:52 -0800

    Automatically identify plugins

--- src/plugin.rs
+++
@@ -1,8 +0,0 @@
-use vidmod_node::Node;
-use vidmod_plugin::Plugin;
-
-pub const SYNC_EXTRACTOR: Plugin = Plugin {
-    make_node: |params| {
-        Node::Intermediate(Box::new(crate::syncextractor::SyncExtractor::new(params)))
-    },
-};
